0ce86e04695294b27c8ada4d0dcfd98a2b69c198
takchan44 <takchan44@gmail.com>
2026-04-14 12:10:48 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
   root: "src",
-  base: "/etf-radar/",   // ← 이 줄 추가
+  base: "/etf-radar/",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
